6636989929dbbfe44cfa3ac3043a3199e622d792
David White <david@runonflux.io>
2024-05-14 13:05:31 +0100

    Update help command

--- files/harden/help.sh
+++ files/harden/help.sh
@@ -4,11 +4,13 @@ Help()
 {
    echo "Available commands"
    echo
-   echo "help       This command."
-   echo "ip         Configure the network."
-   echo "sudo       Only available for the ip command."
-   echo "pwd        Show your current directory."
-   echo "exit       Exit the shell."
+   echo "help           This command."
+   echo "flux-cli       The fluxd cli."
+   echo "fluxbench-cli  The fluxbench cli."
+   echo "ip             Configure the network."
+   echo "sudo           Only available for the ip command."
+   echo "pwd            Show your current directory."
+   echo "exit           Exit the shell."
    echo
 }
 
